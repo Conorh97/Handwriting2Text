9661acedad69cb0065c29c432663435d6aa15fb4
Conor Hanlon <conor.hanlon5@mail.dcu.ie>
2019-04-03 14:07:32 +0100

    Refine image processing

--- src/backend/image.py
+++ src/backend/image.py
@@ -5,6 +5,10 @@ import numpy as np
 DOWNLOAD_DIRECTORY = '{}/tmp-images'.format(os.getcwd())
 
 
+def round_down(num, divisor):
+    return num - (num % divisor)
+
+
 def remove_shadow(image):
     rgb_planes = cv2.split(image)
 
@@ -19,72 +23,22 @@ def remove_shadow(image):
 
 
 def process_image(filename):
-    image = cv2.imread('{}/{}'.format(DOWNLOAD_DIRECTORY, filename))
+    image = cv2.imread('{}/{}'.format(os.getcwd(), filename))
+    image = cv2.resize(image, None, fx=0.5, fy=0.5)
     image = remove_shadow(image)
 
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    th, threshed = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
+    th, threshed = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY_INV)
+    kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (6, 3))
+    dilated = cv2.dilate(threshed, kernel, iterations=2)
 
-    pts = cv2.findNonZero(threshed)
-    rectangle = cv2.minAreaRect(pts)
-
-    (cx, cy) = rectangle[0]
-
-    matrix = cv2.getRotationMatrix2D((cx, cy), 0, 1.0)
-    rotated = cv2.warpAffine(threshed, matrix, (image.shape[1], image.shape[0]))
-    hist = cv2.reduce(rotated, 1, cv2.REDUCE_AVG).reshape(-1)
-
-    th = 9
-    height, width = image.shape[:2]
-
-    uppers = [x for x in range(height - 1) if hist[x] <= th and hist[x + 1] > th]
-    lowers = [x for x in range(height - 1) if hist[x] > th and hist[x + 1] <= th]
-
-    if uppers[0] > lowers[0]:
-        uppers = [0] + uppers
-
-    rotated = cv2.bitwise_not(rotated)
-
+    components = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
     j = 0
-    for i in range(len(lowers)):
-        if abs(uppers[i] - lowers[i]) > 32:
-            cropped = rotated[uppers[i] - 15:lowers[i] + 50, 0:width]
-            cropped_thresh = cv2.threshold(cropped, 127, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-            components = cv2.findContours(cropped_thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
-            word_dims = []
-            for component in components:
-                if cv2.contourArea(component) < 100:
-                    continue
-                (x, y, w, h) = cv2.boundingRect(component)
-                word_dims.append((x, y, w, h))
+    for component in sorted(components, key=lambda c: (round_down(c.tolist()[0][0][1], 40), c.tolist()[0][0][0])):
+        if cv2.contourArea(component) < 300:
+            continue
 
-            word_dims.sort(key=lambda entry: entry[0])
-            words = []
-            start_x = 0
-            x = 0
-            y = 0
-            w = 0
-            h = 0
-            for i, word in enumerate(word_dims):
-                if x == 0 and y == 0 and w == 0 and h == 0:
-                    x, _, w, h = word
-                    start_x = x
-                else:
-                    if word_dims[i][0] - (word_dims[i - 1][0] + word_dims[i - 1][2]) >= 30:
-                        word_image = cropped[y:y + h + 50, start_x - 10:start_x + w + 10]
-                        words.append([(start_x, y, w, h), word_image])
-                        x, _, w, h = word
-                        start_x = x
-                    x = word[0]
-                    w += word[2]
-                    if word[1] < h:
-                        h = word[3]
-                    if word[3] > h:
-                        h = word[3]
-                    if i == len(word_dims) - 1:
-                        word_image = cropped[y:y + h + 50, start_x - 10:start_x + w + 10]
-                        words.append([(start_x, y, w, h), word_image])
-
-            for word in words:
-                cv2.imwrite('{}/{}-{}'.format(DOWNLOAD_DIRECTORY, j, filename), word[1])
-                j += 1
+        (x, y, w, h) = cv2.boundingRect(component)
+        word = image[y - 5 :y + h + 5, x - 5:x + w + 5]
+        cv2.imwrite('{}/{}-{}'.format(DOWNLOAD_DIRECTORY, j, filename), word)
+        j += 1
